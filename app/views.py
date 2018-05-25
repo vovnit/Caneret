8892d775da5bdfb400c7e370aefc4524ffe0d70c
Vladimir <vovnit@phie.ru>
2018-05-26 00:18:14 +0300

    some design huh

---
+++ app/views.py
@@ -0,0 +1,27 @@
+import os
+from flask import render_template, request, send_from_directory
+from werkzeug.utils import secure_filename
+from app import app
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file.filename:
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            print(app.config['UPLOAD_FOLDER'])
+            return render_template('index.html', filename=filename)
+    return render_template('index.html')
+
+
+@app.route('/uploads/<path:filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],
+                               filename, as_attachment=True)
