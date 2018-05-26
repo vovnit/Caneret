55c1d43aab470fac2ed898068da1479edbc2c5ff
Vladimir <vovnit@phie.ru>
2018-05-26 23:59:58 +0300

    music list

--- app/views.py
+++ app/views.py
@@ -5,13 +5,13 @@ from hdfs3 import HDFileSystem
 from werkzeug.utils import secure_filename
 from app import app
 
+
 es = Elasticsearch({'host': 'localhost', 'port': 9200})
-hdfs = HDFileSystem('hdfs://localhost', 8999)
+hdfs = HDFileSystem('hdfs://localhost', 9000)
 
 @app.route('/')
 def index():
     return render_template('index.html')
-
 
 @app.route('/upload', methods=['GET', 'POST'])
 def upload_file():
@@ -26,17 +26,26 @@ def upload_file():
             return render_template('index.html', filename=filename)
     return render_template('upload.html')
 
+def make_audio(path):
+    with hdfs.open(path, replication=1) as music:
+        response = make_response(music.read())
+        response.headers.set('Content-Type', 'audio/mpeg')
+        response.headers.set('Content-Disposition', 'attachment', filename='%s' % path)
+        response.headers.set('Accept-Ranges', 'bytes')
+        return response
+
 
 @app.route('/uploads/<path:filename>')
 def uploaded_file(filename):
+    return make_audio("/tmp/" + filename)
 
-    with hdfs.open("/tmp/" + filename, replication=1) as music:
-        response = make_response(music.read())
-        response.headers.set('Content-Type', 'audio/mpeg')
-        response.headers.set('Content-Disposition', 'attachment', filename='%s' % filename)
-        response.headers.set('Accept-Ranges', 'bytes')
-        return response
     # return send_from_directory(app.config['UPLOAD_FOLDER'],
     #                            filename, as_attachment=True)
 
 
+@app.route('/music')
+def music():
+    res = []
+    for it in hdfs.ls('/tmp/'):
+        res.append(it[5:])
+    return render_template('music.html', music=res)
